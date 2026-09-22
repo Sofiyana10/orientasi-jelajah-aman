01d644e7912ee2c3bfc5a80b0eecc45d3bd4a0fe
Riskha Syofiyana <riskasofiyana10@gmail.com>
2026-09-22 09:52:11 +0700

    feat: tambah SafeAreaView dan responsive layout

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-// app/index.tsx
+// app/(tabs)/index.tsx
 import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useWindowDimensions } from "react-native"; 
+import { SafeAreaView } from "react-native-safe-area-context";
+
 // Catatan: gunakan ../components/ jika file berada di dalam folder app/
 // yang sejajar dengan folder components/
-import RiwayatList from "../../components/RiwayatList";
-import SearchBox from "../../components/SearchBox";
 import WeatherCard from "../../components/WeatherCard";
+import SearchBox from "../../components/SearchBox";
+import RiwayatList from "../../components/RiwayatList";
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
   const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+
+  const { width } = useWindowDimensions(); 
+  const isTablet = width > 768;
 
   // Tambahkan useEffect untuk mencatat perubahan kota aktif
   useEffect(() => {
@@ -24,10 +29,9 @@ export default function HalamanUtama() {
   }
 
   return (
-    <View style={{ padding: 30, paddingTop: 50, gap: 16 }}>
-      <SearchBox onCari={handleCari} />
-      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
-      <RiwayatList daftarKota={riwayat} />
-    </View>
-  );
+  <SafeAreaView style={{ flex: 1, padding: isTablet ? 32 : 16, gap: 16 }}>  <SearchBox onCari={handleCari} /> 
+ <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" /> 
+ <RiwayatList daftarKota={riwayat} /> 
+ </SafeAreaView> 
+ );
 }
--- src/app/(tabs)/pengaturan.tsx
+++ src/app/(tabs)/pengaturan.tsx
@@ -1,11 +1,11 @@
 // app/(tabs)/pengaturan.tsx
-import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native"
 
 export default function TabPengaturan() {
   return (
-    <View style={{ padding: 16 }}>
-      <Text style={{ fontSize: 18, fontWeight: "bold" }}>Jelajah Aman</Text>
-      <Text>Versi 1.0.0</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, padding: 16 }}>
+      <Text>Pengaturan</Text>
+    </SafeAreaView>
   );
 }
